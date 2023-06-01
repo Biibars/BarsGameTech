5380c1986a28d4463e12856610f52932503bd9fd
Biibars <117353527+Biibars@users.noreply.github.com>
2023-06-01 15:08:10 +0600

    Add files via upload

---
+++ srctwo/DataTwo.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "fone",
+    name: "Lenovo",
+    price: 1715,
+    desc: "Lenovo-legion-5pg6",
+    img: "../img/products/f1.jpg",
+  },
+  {
+    id: "ftwo",
+    name: "Asus",
+    price: 670,
+    desc: "Asus-tuf-gaming",
+    img: "../img/products/f2.jpg",
+  },
+  {
+    id: "fthree",
+    name: "Asus",
+    price: 2450,
+    desc: "Asus-roq-ss17",
+    img: "../img/products/f3.jpg",
+  },
+  {
+    id: "ffour",
+    name: "Lenovo",
+    price: 1300,
+    desc: "Lenovo-legion-5",
+    img: "../img/products/f4.jpg",
+  },
+];
